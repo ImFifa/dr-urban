720047cd63497685755ee8dac73cd7992ddf6f92
Filip Urban <fifa.urban@gmail.com>
2020-04-10 13:48:09 +0200

    JS problems solved

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,20 +1,23 @@
-// hide navbar on scroll
-/*
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
+const isBrowser = (typeof window !== `undefined` && typeof document !== `undefined` )
+
+if (isBrowser) {
+  // hide navbar on scroll
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
-*/
-// collapse on link click
-document.addEventListener("click", function(event) {
+
+  // collapse on link click
+  document.addEventListener("click", function(event) {
     if (event.target.classList.contains("nav-link")) {
       document.getElementById("basic-navbar-nav").classList.remove("show");
       document.getElementById('navbar-toggler').classList.add('collapsed');
     }
-});
+  });
+}
