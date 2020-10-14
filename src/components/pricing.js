93ec2b386d9ddc9bfdb9f158fc7f977282312968
Filip Urban <urbanfi4@fit.cvut.cz>
2020-10-14 11:50:56 +0200

    pricing is now dynamic

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,34 +1,37 @@
 import React from 'react'
+import { graphql, useStaticQuery } from "gatsby"
 import { Container } from 'react-bootstrap'
 
-const Pricing = () => (
-    <section className="cenik" id="cenik">
-        <Container>
-        <h2>Ceník</h2>
-        <p>Úkony, které dle Zdravotního řádu, nejsou v souvislosti se zdravotním stavem pacienta a zdravotní pojišťovna je neproplácí. Cena je kalkulovaná dle <a target="_blank"  href="https://www.vzp.cz/poskytovatele/ciselniky/zdravotni-vykony" rel="noopener noreferrer">Sazebníku výkonu VZP</a> a ceny bodu dle MF pro daný rok.</p>
-        
-        <ul>
-            <li>Lékařské vyšetření pro získání řidičského průkazu osoby výdělečně činné: 500 Kč</li>
-            <li>Lékařské vyšetření pro získání řidičského průkazu ostatních osob: 300 Kč</li>
-            <li>Vystavení “Posudku o zdravotní způsobilosti řidiče” pro skupiny C, D, E: 400 Kč</li>
-            <li>Prodloužení platnosti řidičského průkazu důchodce: 200 Kč</li>
-            <li>Vystavení “Průkazu pracovníka v potravinářství”: 300 Kč</li>
-            <li>Lékařské vyšetření pro získání zbrojního průkazu či jeho prodloužení: 500 Kč</li>
-            <li>Lékařské vyšetření pro získání loveckého lístku: 400 Kč</li>
-            <li>Vstupní lékařská prohlídka do zaměstnání: 350 Kč</li>
-            <li>Výstupní lékařská prohlídka ze zaměstnání: 200 Kč</li>
-            <li>Sepsání “Oznámení o úrazu”: 200 Kč</li>
-            <li>Vypracování bodového hodnocení úrazu bolestné či trvalé následky: 250 Kč</li>
-            <li>Lékařské potvrzení – brigáda, sportovní akce, studium na školách: 200 Kč</li>
-            <li>Výpis ze zdravotní dokumentace (například pro závodního lékaře): 250 Kč</li>
-            <li>Vystavení duplikátu PN, propustky, zdravotní dokumentace – dle rozsahu: 100–150 Kč</li>
-            <li>Pozvánka do ordinace, pokud se pacient nedostaví například v pracovní neschopnosti: 50 Kč</li>
-            <li>Potvrzení “Svářečského průkazu” či jiné profesní dokumentace: 300 Kč</li>
-            <li>Předoperační vyšetření nehrazené z veřejného zdravotního pojištění: 500 Kč + výdaje na laboratorní vyšetření</li>
-            <li>Očkování nehrazené z veřejného zdravotního pojištění: 100 Kč</li>
-        </ul>
-        </Container>
-    </section>
-)
+const Pricing = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        pricing: markdownRemark(fields: {slug: {eq: "/content/pricing/"}}) {
+          frontmatter {
+            items {
+              name
+              price
+            }
+            title
+            description
+          }
+        }
+      }
+  `)
+
+    return (
+        <section className="cenik" id="cenik">
+            <Container>
+            <h2>{data.pricing.frontmatter.title}</h2>
+            <p>{data.pricing.frontmatter.description}</p>
+            
+            <ul>
+                {data.pricing.frontmatter.items.map(({name, price}) =>
+                    <li>{name} {price}</li>
+                )}
+            </ul>
+            </Container>
+        </section>
+    )
+}
 
 export default Pricing
